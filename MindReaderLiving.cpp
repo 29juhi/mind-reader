f18df53d7975bfdf196afc4fbd3ef967df6f96bb
29juhi <29juhi@gmail.com>
2024-06-27 19:23:02 +0530

    Add files via upload

---
+++ MindReaderLiving.cpp
@@ -0,0 +1,892 @@
+#include <iostream>
+using namespace std;
+
+
+
+bool canfly()
+{
+    bool x;
+    cout << "can it fly?";
+    cin >> x;
+    return x;
+}
+bool isgreen()
+{
+    cout << "is it green in color?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool colorful()
+{
+    cout << "is it colourful or other than black/brown?";
+    bool x;
+    cin >> x;
+    return x;
+} // butterfly, bee, draginfly, beetle, ladybug, wasp, firefly (ccan be of color other than black)
+bool isinsect()
+{
+    cout << "is it an insect?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool isred()
+{
+    cout << "is it red in color?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool biteorsting()
+{
+    cout << "can it bite or sting?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool glows()
+{
+    cout << "does it glow at night?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool hives()
+{
+    cout << "do they live in hives?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool ispretty()
+{
+    cout << "is it really pretty?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool startwithm()
+{
+    cout << "does its name start with m?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool eatwood()
+{
+    cout << "does it eat wood?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool haslegs()
+{
+    cout << "does it have legs?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool eightlegs()
+{
+    cout << "does it have 8 legs?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool hundredlegs()
+{
+    cout << "does it have 100 legs?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool colonies()
+{
+    cout << "does it live in colonies?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool migratory()
+{
+    cout << "is it a migratory bird?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool water()
+{
+    cout << "is it usually found near water?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool vsmallncolor()
+{
+    cout << "is it very very small and very colourful?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool crow()
+{
+    cout << "is it compleatly black?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool night()
+{
+    cout << "is it usually visible at night?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool woodpecker()
+{
+    cout << "does it peck wood?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool blue()
+{
+    cout << "is it usually bluish grey in color?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool greenbird()
+{
+    cout << "is it green colored?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool sparrow()
+{
+    cout << "is it small in size and brown?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool startswithe()
+{
+    cout << "does its name start with e?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool bigsize()
+{
+
+    cout << "is it large in size?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool swan()
+{
+    cout << "is it very pretty and all white always with black beak?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool penguin()
+{
+    cout << "is it usually found towards the poles?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool isbird()
+{
+    cout << "is it a bird?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool kingfisher()
+{
+
+    cout << "does its name has fish in it?";
+    bool x;
+    cin >> x;
+    return x;
+}
+
+bool isanimal()
+{
+    cout << "is it an animal?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool land()
+{
+    cout << "is it a land animal?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool mammal()
+{
+    cout << "is it a mammal?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool fourlegs()
+{
+    cout << "does it have 4 legs?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool wild()
+{
+    cout << "is it a wild animal?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool herbi()
+{
+    cout << "is it a herbivore?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool trunk()
+{
+    cout << "does it have a trunk?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool neck()
+{
+    cout << "does it have a long neck?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool bnw()
+{
+    cout << "is it black and white in color?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool stripes()
+{
+    cout << "does it have stripes?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool red()
+{
+    cout << "is it red?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool dogfam()
+{
+    cout << "is it dog family?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool wolf()
+{
+    cout << "is it large in size?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool pet()
+{
+    cout << "can it be kept as pet?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool bark()
+{
+    cout << "does it bark";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool meow()
+{
+    cout << "does it meow?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool race()
+{
+    cout << "is it used in races?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool dairy()
+{
+    cout << "is it used for dairy?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool wool()
+{
+    cout << "is it used for wool?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool little()
+{
+    cout << "is it very little in size?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool fat()
+{
+    cout << "is it fat?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool twolegs()
+{
+    cout << "does it have 2 legs?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool sack()
+{
+
+    cout << "does it have a pocket in body?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool fins()
+{
+    cout << "does it have fins?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool largest()
+{
+    cout << "is it large in size?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool friendly()
+{
+    cout << "is it friendly with humans?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool sharpteeth()
+{
+    cout << "does it have sharp teeth";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool seahorse()
+{
+    cout << "does it have the name horse in it?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool tentacles()
+{
+    cout << "does it have 8 tentacles?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool claws()
+{
+    cout << "does it have claws?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool sand()
+{
+    cout << "is it seen on sand?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool star()
+{
+    cout << "is it star shaped?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool jump()
+{
+    cout << "it jumps well?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool domastic()
+{
+    cout << "can it be domestic?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool scavenger()
+{
+    cout << "is it scavenger?";
+    bool x;
+    cin >> x;
+    return x;
+}
+bool landandwater(){
+    cout<<"found on both land and water";
+    bool x;
+    cin>>x;
+    return x;
+}
+bool shell(){
+    cout<<"does it have shell?";
+    bool x;
+    cin>>x;
+    return x;
+
+}
+bool horns(){
+    cout<<"does it have horns?";
+    bool x;
+    cin>>x;
+    return x;
+}
+
+
+int isliving(){
+    if (isinsect())
+    {
+        if (canfly())
+        {
+            if (colorful())
+            {
+                if (isgreen())
+                {
+                    cout << "grasshopper";
+                }
+                else
+                {
+                    if (isred())
+                    {
+                        cout << "ladybug";
+                    }
+                    else if (ispretty())
+                    {
+                        cout << "butterfly";
+                    }
+                    else if (glows())
+                    {
+                        cout << "firefly";
+                    }
+                    else if (biteorsting())
+                    {
+                        if (hives())
+                        {
+                            cout << "bees";
+                        }
+                        else
+                        {
+                            cout << "beetles";
+                        }
+                    }
+
+                    else if (startwithm())
+                    {
+                        cout << "moth";
+                    }
+                    else
+                    {
+                        cout << "dragonfly";
+                    }
+                }
+            }
+            else
+            {
+                if (eatwood())
+                {
+                    cout << "termite";
+                }
+                else if (biteorsting())
+                {
+                    cout << "mosquito";
+                }
+                else
+                {
+                    cout << "fly";
+                }
+            }
+        }
+
+        else
+        {
+            if (isgreen())
+            {
+
+                cout << "catterpiller";
+            }
+            else
+            {
+                if (haslegs())
+                {
+                    if (eightlegs())
+                    {
+                        cout << "spider";
+                    }
+                    else if (hundredlegs())
+                    {
+                        cout << "centipede";
+                    }
+                    else if (colonies())
+                    {
+                        cout << "ant";
+                    }
+                    else
+                    {
+                        cout << "cockroach";
+                    }
+                }
+                else
+                {
+                    if (shell()){
+                        cout << "snail";
+
+                    }
+                    else{
+                        cout<<"earthworm";
+                    }
+                    
+                    
+                }
+            }
+        }
+    }
+    else if (isbird())
+    {
+        if (night())
+        {
+            cout << "owl";
+        }
+        else
+        {
+            if (migratory())
+            {
+                cout << "seagull";
+            }
+            else if (mammal())
+            {
+                cout << "bat";
+            }
+            else if (water())
+            {
+                if (swan())
+                {
+                    cout << "swan";
+                }
+                else if (kingfisher())
+                {
+                    cout << "kingfisher";
+                }
+                else if (penguin())
+                {
+                    cout << "penguin";
+                }
+                else
+                {
+                    cout << "duck";
+                }
+            }
+            else if (bigsize())
+            {
+                if (blue())
+                {
+                    cout << "pegion";
+                }
+                else if (scavenger())
+                {
+                    cout << "vulture";
+                }
+                else if (greenbird())
+                {
+                    cout << "parrot";
+                }
+                else if (crow())
+                {
+                    cout << "crow";
+                }
+                else if (woodpecker())
+                {
+                    cout << "woodpecker";
+                }
+                else if (startswithe())
+                {
+                    cout << "eagle";
+                }
+                else
+                {
+                    cout << "hawk";
+                }
+            }
+            else
+            {
+                if (vsmallncolor())
+                {
+                    cout << "humming bird";
+                }
+                else if (domastic())
+                {
+                    cout << "chicken";
+                }
+
+                else if (blue())
+                {
+                    cout << "pegion";
+                }
+                else if (greenbird())
+                {
+                    cout << "parrot";
+                }
+                else
+                {
+                    cout << "sparrow";
+                }
+            }
+        }
+    }
+    else if (isanimal())
+    {
+        if (land())
+        {
+            if (fourlegs())
+            {
+                if (wild())
+                {
+                    if (herbi())
+                    {
+                        if (trunk())
+                        {
+                            cout << "elephant";
+                        }
+                        else if (neck())
+                        {
+                            cout << "giraffe";
+                        }
+                        else if (bnw())
+                        {
+                            if (stripes())
+                            {
+                                cout << "zebra";
+                            }
+                            else
+                            {
+                                cout << "panda";
+                            }
+                        }
+                        else if (red())
+                        {
+                            cout << " red panda";
+                        }
+                        else if (horns()){
+                            cout<<"rhinoceros";
+                        }
+                        else
+                        {
+                            cout << "koala";
+                        }
+                    }
+                    else
+                    {
+                        if (dogfam())
+                        {
+                            if (scavenger())
+                            {
+                                cout << "hyena";
+                            }
+                            else if (wolf())
+                            {
+                                cout << "wolf";
+                            }
+
+                            else
+                            {
+                                cout << "fox";
+                            }
+                        }
+                        else
+                        {
+                            if (stripes())
+                            {
+                                cout << "tiger";
+                            }
+                            else
+                            {
+                                cout << "lion";
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (pet())
+                    {
+                        if (bark())
+                        {
+                            cout << "dog";
+                        }
+                        else if (meow())
+                        {
+                            cout << "cat";
+                        }
+                        else
+                        {
+                            cout << "rabbit";
+                        }
+                    }
+                    else
+                    {
+                        if (race())
+                        {
+                            cout << "horse";
+                        }
+                        else if (dairy())
+                        {
+                            cout << "cow";
+                        }
+                        else if (wool())
+                        {
+                            cout << "sheep";
+                        }
+                        else if (little())
+                        {
+                            cout << "mouse";
+                        }
+                        else if (fat())
+                        {
+                            cout << "pig";
+                        }
+                        else
+                        {
+                            cout << "goat";
+                        }
+                    }
+                }
+            }
+            else if (twolegs())
+            {
+                if (sack())
+                {
+                    cout << "kangaroo";
+                }
+                else
+                {
+                    cout << "humans";
+                }
+            }
+            else
+            {
+                cout << "snake";
+            }
+        }
+        else if (landandwater()){
+            if (sharpteeth()){
+                cout<<"alligator / crocodile";
+            }
+            else if (fat()){
+                cout<<"hippopotamus";
+            }
+            else{
+                cout<<"frog";
+            }
+
+        }
+        else
+        {
+            if (fins())
+            {
+                if (largest())
+                {
+                    cout << "whale";
+                }
+                else if (friendly())
+                {
+                    cout << "dolphin";
+                }
+                else if (sharpteeth())
+                {
+                    cout << "shark";
+                }
+                else if (seahorse())
+                {
+                    cout << "seahorse";
+                }
+                else
+                {
+                    cout << "fish";
+                }
+            }
+            else
+            {
+                if (tentacles())
+                {
+                    cout << "octopus";
+                }
+                else if (claws())
+                {
+                    if (sand())
+                    {
+                        cout << "crab";
+                    }
+                    else
+                    {
+                        cout << "lobster";
+                    }
+                }
+                else if (star())
+                {
+                    cout << "starfish";
+                }
+                else
+                {
+                    cout << "jellyfish";
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    cout << "Think of any animal, bird or insect but try to keep it simple. by asking a few question i will read your mind!"<<endl<<"enter 0 if true and 1 if false"<<endl;
+    isliving();
+    return 0;
+}
